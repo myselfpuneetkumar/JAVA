bbba190ffc7a3cd49ce1e8e529af645adfc0813c
Puneet Kumar <136183923+myselfpuneetkumar@users.noreply.github.com>
2023-11-05 23:08:39 +0530

    Area_Of_Circle.java

--- Area_Of_Circle.java
+++ Area_Of_Circle.java
@@ -3,11 +3,10 @@ public class Area_Of_Circle{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Please Enter the Radius of Circle ");
-        //Here rd means radius and ar is radius multiply radius.
+        //Here rd means radius and arr is radius multiply radius.
         int rd=sc.nextInt();
         double pi=3.14;
         double arr=pi*rd*rd;
-        //int arr=ar*pi;
         System.out.println("The Area of Circle is:"+arr);
         sc.close();
         
